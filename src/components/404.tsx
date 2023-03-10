df0ab04248d80d5b76e89ef0fb15282adf69c881
Eflried <elfriedv16@gmail.com>
2023-03-10 23:17:52 +0100

    Update 404 Component

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
 
   return (
-    <div className='w-75 mx-auto my-5'>
+    <div className='w-50 mx-auto text-black' style={{margin : '100px 0'}}>
         <img src="/src/assets/img/404.png" alt="" className="w-100 h-100" />
+        <div className="text-center fw-bold">
+          <h1 className="mt-3">Oops! Page Not Found.</h1>
+          <p className="opacity-75 my-4">The page you are looking for is not avaible or doesn't belong to this website!</p>
+          <Link to={"/"} className='fd-btn w-25 mx-auto'><i className="bi bi-link-45deg me-2"></i>GO TO HOME</Link>
+        </div>
     </div>
   )
 
