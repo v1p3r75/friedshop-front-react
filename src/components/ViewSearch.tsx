d5bfae6b5ee9130576dcd1fd457f294a2994e781
Eflried <elfriedv16@gmail.com>
2023-03-12 00:22:23 +0100

    Update View Search Component

--- src/components/ViewSearch.tsx
+++ src/components/ViewSearch.tsx
@@ -26,7 +26,7 @@ const ViewSearch = ({ setShow }: { setShow: Function }) => {
                 <div className="overflow-auto">
                     <form action="" method="post" className='w-100 d-flex'>
                         <div className='w-75'><input type="text" name="searchquery" placeholder='Search Here ...' className='p-3 border border-1 rounded-0 w-100' /></div>
-                        <div className='w-25'><button type='submit' className='btn-outline-none border-0 fd-bg-primary rounded-0 py-3 px-4'><i className="bi bi-search w-100"></i></button></div>
+                        <div className='w-25'><button type='submit' className='btn-outline-none border-0 fd-bg-primary rounded-0 py-3 px-4 text-white'><i className="bi bi-search w-100"></i></button></div>
                     </form>
                 </div>
                 <div className='search-category opacity-75 mt-4'>
@@ -37,9 +37,6 @@ const ViewSearch = ({ setShow }: { setShow: Function }) => {
                     <div className="popular-search my-5">
                         <h6>Popular Searchs</h6>
                         <SearchCategory type="recent" />
-                    </div>
-                    <div className="popular-category">
-                        <h6>Popular Category</h6>
                     </div>
                 </div>
             </div>
