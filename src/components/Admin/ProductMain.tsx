2dd6c34cb28f890328af4ec44468e333c2a37baa
Eflried <elfriedv16@gmail.com>
2023-03-13 15:16:51 +0100

    Add Admin Products List Page

--- src/components/Admin/ProductMain.tsx
+++ src/components/Admin/ProductMain.tsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import { productsTest } from '../../views/VitualData'
+
+const CreateProduct = () => {
+
+  return (
+  <div className="table-responsive">
+        <table className="table table-default text-center table-bordered">
+            <thead>
+                <tr className='fd-bg-primary text-white'>
+                    <th scope="col" className='p-3'>IMAGE</th>
+                    <th scope="col" className='p-3'>PRODUCT NAME</th>
+                    <th scope="col" className='p-3'>PRICE</th>
+                    <th scope="col" className='p-3'>TOTAL STOCK</th>
+                    <th scope="col" className='p-3'>ACTION</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    productsTest.map(product => {
+                        return (
+                            <tr className="p-3">
+                                <td scope="row w-25"><img src={product.img} alt={product.name} style={{width : '50px', height : '50px'}}/></td>
+                                <td className='fw-bold'>{product.name}</td>
+                                <td>{product.price}</td>
+                                <td>{45}</td>
+                                <td className='fw-bold d-flex gap-2 justify-content-center'>
+                                  <a href="#" className='p-2 rounded-2 fd-bg-primary'><i className="bi bi-eye"></i></a>
+                                  <a href="#" className='p-2 rounded-2 bg-secondary'><i className="bi bi-pen"></i></a>
+                                  <a href="#" className='p-2 rounded-2 bg-danger'><i className="bi bi-trash"></i></a>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    </div>
+  );
+}
 
 const ProductMain = () => {
   return (
     <div className='text-black'>
         <h4 className="fw-bold">List of products</h4>
+        <div className="add-product my-3 d-flex justify-content-end">
+          <a href="#" className="fd-btn bg-secondary w-25 text-center rounded-3">ADD PRODUCT</a>
+        </div>
+        <div className="subPartMain">
+          <CreateProduct />
+        </div>
     </div>
   )
 }
--- src/views/Admin/AdminPanel.tsx
+++ src/views/Admin/AdminPanel.tsx
@@ -9,10 +9,9 @@ import RoutePaths from '../../config'
 const AdminPanel = ({currentComponent} : {currentComponent : React.ReactNode}) => {
 
   const navigation = useLocation();
-  const path = navigation.pathname;
 
   useEffect(() => {
-    document.querySelectorAll('html,body, #root').forEach((e) => e.classList.add('h-100'));
+    document.querySelectorAll('html,body, #root').forEach((e) => e.classList.add('h-100' ,'overflow-hidden'));
   }, [])
 
     
@@ -20,7 +19,7 @@ const AdminPanel = ({currentComponent} : {currentComponent : React.ReactNode}) =
     <>
       <div className="admin-container h-100">
         <AdminHeader />
-        <div className='admin-section d-flex justify-content-between gap-3 px-5 my-5 h-75'>
+        <div className='admin-section d-flex justify-content-between gap-4 px-5 my-4 h-75'>
               <aside className='user-page w-25 fw-bold border border-1'>
                   <div><Link to={RoutePaths.admin.path} className={toggleLinkClass(RoutePaths.admin.path)}>Dashboard<i className="bi bi-house float-end"></i></Link></div>
                   <div><Link to={RoutePaths.adminProducts.path}  className={toggleLinkClass(RoutePaths.adminProducts.path)}>Products<i className="bi bi-newspaper float-end"></i></Link></div>
@@ -30,7 +29,7 @@ const AdminPanel = ({currentComponent} : {currentComponent : React.ReactNode}) =
                   <div><Link to={RoutePaths.admin.path}  className={toggleLinkClass('/admin/blog')}>Blog<i className="bi bi-book float-end"></i></Link></div>
                   <div><Link to={RoutePaths.admin.path}  className={toggleLinkClass('/admin/accounts')}>Account<i className="bi bi-person float-end"></i></Link></div>
               </aside>
-              <div className="w-75">
+              <div className="w-75 overflow-auto">
                 {currentComponent}
               </div>
           </div>
