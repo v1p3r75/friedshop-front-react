44c69b6cc3e2dd9338d50e0350aaf05586d519fb
v1p3r75 <elfriedv16@gmail.com>
2023-04-25 23:02:30 +0100

    Show NotFound page if api send  404 code

--- src/views/ViewProduct.tsx
+++ src/views/ViewProduct.tsx
@@ -3,6 +3,7 @@ import Footer from './includes/Footer';
 import Header from './includes/Header';
 import Banner from '../components/Banner';
 import Spinner from '../components/Spinner';
+import NotFound from "../components/404";
 import ProductDetails from '../components/ProductDetails';
 import { useParams } from 'react-router-dom';
 import { useGetProductQuery } from "../store/apiquery/productApiSlice"
@@ -18,9 +19,9 @@ const ViewProduct = () => {
         <Header />
         <Banner page="Shop Details" path={["Home", "Shop Details"]}/>
         {
-          !isLoading ? <ProductDetails product={details.data}/> : <Spinner />
+          !isLoading && isSuccess ? <ProductDetails product={details.data}/> : 
+          (isError ? <NotFound /> : <Spinner />)
         }
-        
         <Footer />
     </>
   )
