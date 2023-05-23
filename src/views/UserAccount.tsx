98e3158b9b2000871b108af25e753c7ea4faa02d
v1p3r75 <elfriedv16@gmail.com>
2023-05-23 19:17:01 +0100

    get commands from api

--- src/views/UserAccount.tsx
+++ src/views/UserAccount.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState, SyntheticEvent } from 'react'
+import { useEffect, useState, SyntheticEvent } from 'react'
 import Header from './includes/Header'
 import Footer from './includes/Footer'
 import { Link, useNavigate } from 'react-router-dom'
@@ -11,6 +11,7 @@ import { logoutCurrentUser, setUser } from '../store/userSlice'
 import LoadingButton from '../components/LoadingButton'
 import { HandleResult } from '../components/HandleResult'
 import Swal from 'sweetalert2'
+import { useGetAllCommandsQuery } from '../store/apiquery/CommandApiSlice'
 
 export const UserDashboard = () => {
     return (
@@ -22,53 +23,41 @@ export const UserDashboard = () => {
 }
 
 export const UserOrders = () => {
+
+    const { data: commands, isLoading } = useGetAllCommandsQuery("api/commands");
+
     return (
         <div className="user-orders p-3 border border-2 text-black">
             <h3>Orders</h3>
             <div className="table-responsive">
-                <table className="table table-default table-bordered text-center">
-                    <thead>
-                        <tr>
-                            <th scope="col">Commande</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Total</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="">
-                            <td scope="row">1</td>
-                            <td>01 Dec 2023</td>
-                            <td>R1C3</td>
-                            <td>$250</td>
-                            <td><a href="#" className='text-info'>Voir</a></td>
-                        </tr>
-                        <tr>
-                            <td scope="row">2</td>
-                            <td>01 Dec 2023</td>
-                            <td>R1C3</td>
-                            <td>$250</td>
-                            <td><a href="#" className='text-info'>Voir</a></td>
-                        </tr>
-                        <tr>
-                            <td scope="row">3</td>
-                            <td>01 Dec 2023</td>
-                            <td>R1C3</td>
-                            <td>$250</td>
-                            <td><a href="#" className='text-info'>Voir</a></td>
-                        </tr>
-                    </tbody>
-                </table>
+                {
+                    isLoading ? 
+                    <table className="table table-default table-bordered text-center">
+                        <thead>
+                            <tr>
+                                <th scope="col">Commande</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Total</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table> :
+
+                    <Spinner />
+                }
             </div>
-            
+
         </div>
     )
 }
 
 export const UserAddress = () => {
 
-    const user : User = useAppSelector(state => state.user);
+    const user: User = useAppSelector(state => state.user);
 
     return (
         <div className="user-address p-3 border border-2 text-black">
@@ -83,17 +72,17 @@ export const UserAddress = () => {
 
 export const UserDetails = () => {
 
-    const user : User = useAppSelector(state => state.user);
+    const user: User = useAppSelector(state => state.user);
     const [data, setData] = useState(user);
     const [updateUser, result] = useUpdateUserMutation();
 
-    const handleChange = ( e : SyntheticEvent) => {
+    const handleChange = (e: SyntheticEvent) => {
 
-        const target : HTMLInputElement = e.target
-        setData({...data, [target.name]: target.value });
+        const target: HTMLInputElement = e.target
+        setData({ ...data, [target.name]: target.value });
     }
 
-    const handleSubmit = (e : SyntheticEvent) => {
+    const handleSubmit = (e: SyntheticEvent) => {
 
         e.preventDefault();
         updateUser(data);
@@ -104,62 +93,62 @@ export const UserDetails = () => {
         <div className="user-edit-details p-3 border border-2 text-black">
             <h3>Account Details</h3>
             <form action="" method="post" className="checkout-service p-3" onSubmit={handleSubmit}>
-                    <div className='d-flex gap-2'>
-                        <label className='w-50'>
-                            <span>First Name</span>
-                            <input type="text" name="firstname" className="form-control w-100 rounded-0 p-2" value={data.firstname} onChange={handleChange}/>
-                        </label>
-                        <label className='w-50'>
-                            <span>Last Name</span>
-                            <input type="text" name="lastname" className="form-control w-100 rounded-0 p-2" value={data.lastname} onChange={handleChange}/>
-                        </label>
-                    </div>
-                    <div className='my-4'>
-                        <label className='w-100'>
-                            <span>Email</span>
-                            <input type="email" name="email" className="form-control w-100 rounded-0 p-2" value={data.email} onChange={handleChange}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className='w-100'>
-                            <span>Address</span>
-                            <input type="text" name="address" className="form-control w-100 rounded-0 p-2" value={data.address} onChange={handleChange}/>
-                        </label>
-                    </div>
-                    <div className='my-4'>
-                        <label className='w-100'>
-                            <span>Password</span>
-                            <input type="password" name="password" className="form-control w-100 rounded-0 p-2" value={user.password} onChange={handleChange}/>
-                        </label>
-                    </div>
-                    <div className='my-4'>
-                        <HandleResult result={result} />
-                    </div>
-                    <LoadingButton loadingState={result.isLoading}>
-                        <div className='mt-4'><button type="submit" className="fd-btn border-0 w-100 text-center">SAVE NOW</button></div>
-                    </LoadingButton>
-                    
-                </form>
+                <div className='d-flex gap-2'>
+                    <label className='w-50'>
+                        <span>First Name</span>
+                        <input type="text" name="firstname" className="form-control w-100 rounded-0 p-2" value={data.firstname} onChange={handleChange} />
+                    </label>
+                    <label className='w-50'>
+                        <span>Last Name</span>
+                        <input type="text" name="lastname" className="form-control w-100 rounded-0 p-2" value={data.lastname} onChange={handleChange} />
+                    </label>
+                </div>
+                <div className='my-4'>
+                    <label className='w-100'>
+                        <span>Email</span>
+                        <input type="email" name="email" className="form-control w-100 rounded-0 p-2" value={data.email} onChange={handleChange} />
+                    </label>
+                </div>
+                <div>
+                    <label className='w-100'>
+                        <span>Address</span>
+                        <input type="text" name="address" className="form-control w-100 rounded-0 p-2" value={data.address} onChange={handleChange} />
+                    </label>
+                </div>
+                <div className='my-4'>
+                    <label className='w-100'>
+                        <span>Password</span>
+                        <input type="password" name="password" className="form-control w-100 rounded-0 p-2" value={user.password} onChange={handleChange} />
+                    </label>
+                </div>
+                <div className='my-4'>
+                    <HandleResult result={result} />
+                </div>
+                <LoadingButton loadingState={result.isLoading}>
+                    <div className='mt-4'><button type="submit" className="fd-btn border-0 w-100 text-center">SAVE NOW</button></div>
+                </LoadingButton>
+
+            </form>
         </div>
     )
 }
 
-const UserAccount = ({currentComponent = <UserDashboard />} : {currentComponent? : React.ReactNode}) => {
-    
+const UserAccount = ({ currentComponent = <UserDashboard /> }: { currentComponent?: React.ReactNode }) => {
+
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
 
-    const logoutUser = (e : SyntheticEvent) => {
+    const logoutUser = (e: SyntheticEvent) => {
         e.preventDefault();
         Swal.fire({
-			title: 'Are you sure?',
-			text: "Are you sure to delete this product ?",
-			icon: 'warning',
-			showCancelButton: true,
-			confirmButtonColor: '#3085d6',
-			cancelButtonColor: '#d33',
-			confirmButtonText: 'Yes, Logout it!'
-		}).then((r) => {
+            title: 'Are you sure?',
+            text: "Are you sure to delete this product ?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Logout it!'
+        }).then((r) => {
             if (r.isConfirmed) {
                 removeItem('_token');
                 removeItem('user');
@@ -168,25 +157,25 @@ const UserAccount = ({currentComponent = <UserDashboard />} : {currentComponent?
             }
         })
     }
-    
+
     return (
-    <>
-        <Header />
-        <div className='d-flex justify-content-between gap-3 px-5 my-5'>
-            <aside className='user-page w-25 fw-bold border border-1 h-25'>
-                <div><Link to={RoutePaths.userAccount} className={toggleLinkClass(RoutePaths.userAccount)}>Dashboard<i className="bi bi-house float-end"></i></Link></div>
-                <div><Link to={RoutePaths.userOrders} className={toggleLinkClass(RoutePaths.userOrders)}>Orders<i className="bi bi-bookmark-fill float-end"></i></Link></div>
-                <div><Link to={RoutePaths.userAdress} className={toggleLinkClass(RoutePaths.userAdress)}>Address<i className="bi bi-envelope float-end"></i></Link></div>
-                <div><Link to={RoutePaths.userDetails} className={toggleLinkClass(RoutePaths.userDetails)}>Account Details<i className="bi bi-person float-end"></i></Link></div>
-                <div><a href='#' className="d-block p-3 text-black" onClick={logoutUser}>Logout<i className="bi bi-person-slash float-end"></i></a></div>
-            </aside>
-            <div className="w-75 mt-3">{currentComponent}</div>
-            {/* // !isFetching && !isError ? <div className="w-75 mt-3">{currentComponent}</div> :
+        <>
+            <Header />
+            <div className='d-flex justify-content-between gap-3 px-5 my-5'>
+                <aside className='user-page w-25 fw-bold border border-1 h-25'>
+                    <div><Link to={RoutePaths.userAccount} className={toggleLinkClass(RoutePaths.userAccount)}>Dashboard<i className="bi bi-house float-end"></i></Link></div>
+                    <div><Link to={RoutePaths.userOrders} className={toggleLinkClass(RoutePaths.userOrders)}>Orders<i className="bi bi-bookmark-fill float-end"></i></Link></div>
+                    <div><Link to={RoutePaths.userAdress} className={toggleLinkClass(RoutePaths.userAdress)}>Address<i className="bi bi-envelope float-end"></i></Link></div>
+                    <div><Link to={RoutePaths.userDetails} className={toggleLinkClass(RoutePaths.userDetails)}>Account Details<i className="bi bi-person float-end"></i></Link></div>
+                    <div><a href='#' className="d-block p-3 text-black" onClick={logoutUser}>Logout<i className="bi bi-person-slash float-end"></i></a></div>
+                </aside>
+                <div className="w-75 mt-3">{currentComponent}</div>
+                {/* // !isFetching && !isError ? <div className="w-75 mt-3">{currentComponent}</div> :
                 // <Spinner /> */}
-        </div>
-        <Footer/>
-    </>
-  )
+            </div>
+            <Footer />
+        </>
+    )
 }
 
 export default UserAccount
