c20c193d6b8b74dc836a3efc479d8c75a7987694
v1p3r75 <elfriedv16@gmail.com>
2023-06-01 14:23:19 +0100

    Date delay animation for deal of the day

--- src/components/DateCount.tsx
+++ src/components/DateCount.tsx
@@ -1,27 +1,72 @@
-import React, { FC, SyntheticEvent, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type OptionsType = {
-    initialValue? : number;
-    step? : number;
-    min? : number,
-    max? : number
-} 
-
-const DateCount = () => {
-    
-    const [date, setDate] = useState<Date>(new Date());
-    
-    return (
-        <div className="d-flex gap-2 text-center my-3">
-            <div className='fd-bg-primary py-1 px-3 bg-black fw-bold'><span className='text-white'>-84</span><br /><span className='fd-color-primary'>DAYS</span></div>
-            <div className='fd-bg-primary py-1 px-3 bg-black fw-bold'><span className='text-white'>-07</span><br /><span className='fd-color-primary'>HOURS</span></div>
-            <div className='fd-bg-primary py-1 px-3 bg-black fw-bold'><span className='text-white'>-14</span><br /><span className='fd-color-primary'>MINUTES</span></div>
-            <div className='fd-bg-primary py-1 px-3 bg-black fw-bold'><span className='text-white'>-10</span><br /><span className='fd-color-primary'>SEC</span></div>
-        </div>
-    )
-
+    initialValue?: number;
+    step?: number;
+    min?: number,
+    max?: number
 }
 
+const DateCount = () => {
 
+    const expiredDay = 3 * 24 * 60 * 60 * 1000;
 
-export default DateCount
+    const currentDate = new Date();
+
+    const targetDate = new Date(currentDate.getTime() + expiredDay);
+
+    const [remainingTime, setRemainingTime] = useState(calculateRemainingTime());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setRemainingTime(calculateRemainingTime());
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    function calculateRemainingTime() {
+        const diff = targetDate.getTime() - new Date().getTime();
+
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+        return {
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    }
+
+    return (
+        <div className="d-flex gap-2 text-center my-3">
+            <div className="fd-bg-primary py-1 px-3 bg-black fw-bold">
+                <span className="text-white">-{remainingTime.days}</span>
+                <br />
+                <span className="fd-color-primary">DAYS</span>
+            </div>
+            <div className="fd-bg-primary py-1 px-3 bg-black fw-bold">
+                <span className="text-white">-{remainingTime.hours}</span>
+                <br />
+                <span className="fd-color-primary">HOURS</span>
+            </div>
+            <div className="fd-bg-primary py-1 px-3 bg-black fw-bold">
+                <span className="text-white">-{remainingTime.minutes}</span>
+                <br />
+                <span className="fd-color-primary">MINUTES</span>
+            </div>
+            <div className="fd-bg-primary py-1 px-3 bg-black fw-bold">
+                <span className="text-white">-{remainingTime.seconds}</span>
+                <br />
+                <span className="fd-color-primary">SEC</span>
+            </div>
+        </div>
+    );
+};
+
+export default DateCount;
